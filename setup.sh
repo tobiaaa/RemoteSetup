eeb6d793faa87678548ae18673ddbec420bcb1a2
Tobias Raichle <st164519@stud.uni-stuttgart.de>
2023-12-14 14:40:39 +0100

    feat: Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#! bin/sh
+
+echo "Setting up Remote compute node";
+
+mkdir data
+mkdir src
+
+echo "Enter GitHub Username";
+read git_user;
+git config --global user.name $git_user;
+
+echo "Enter GitHub E-Mail";
+read git_mail;
+git config --global user.email $git_mail;
+
